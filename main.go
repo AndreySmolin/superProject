c2bb1600d3b88d68679749af409f6ebdf7a04d3a
Andrey Smolin <saselitasm@yandex.ru>
2025-11-26 20:05:55 +0300

    bug fixes

--- main.go
+++ main.go
@@ -11,14 +11,14 @@ import (
 func main() {
 	x := rand.Intn(100)
 	scanner := bufio.NewScanner(os.Stdin)
-el:
+
 	for i := 0; i < 10; i++ {
 		fmt.Println("Введите число:")
 		scanner.Scan()
 		y, _ := strconv.Atoi(scanner.Text())
 		if x == y {
 			fmt.Printf("Число угадано количество попыток: %d", i)
-			break el
+			return
 		} else if x > y {
 			fmt.Println("Заданное число больше ")
 		} else {
